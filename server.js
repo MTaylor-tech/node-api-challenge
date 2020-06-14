5ca80e4236d8378cea5e1718e5a7882e6e7b5dac
Matthew Taylor <majikpig@gmail.com>
2020-06-14 19:25:22 -0400

    added server.js, wrote index.js, built out organization

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+const projectRouter = require('./routers/projectRouter')
+const actionRouter = require('./routers/actionRouter')
+const logger = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+
+const logStyle = process.env.LOGSTYLE || 'short'
+const welcomeMessage = process.env.WELCOME || `<h2>Welcome to the Projects/Actions API</h2>`
+
+const server = express()
+
+server.use(cors())
+server.use(express.json())
+
+server.use(logger(logStyle))
+
+server.get('/', (req, res) => {
+  res.send(welcomeMessage);
+})
+
+server.use('/users', userRouter)
+server.use('/posts', postRouter)
+server.use(errorHandler())
+
+module.exports = server;
